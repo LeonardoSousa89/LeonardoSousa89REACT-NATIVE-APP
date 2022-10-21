b188e676733dc26ecaea4f76a2cd0797c90c95e1
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-10-21 11:18:41 -0300

    app @v1.0.4

---
+++ app @v1.0.5/registroDeMutantes/src/views/cadastro/index.jsx
@@ -0,0 +1,134 @@
+import { TextInput,Button } from '@react-native-material/core';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import { auth, db } from '../../services'
+
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+import { addDoc, collection } from 'firebase/firestore';
+
+import Title from '../../components/titulo'
+
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+
+export default (props) =>{
+  
+  const [nome, setNome]=useState('')
+  const [email, setEmail]=useState('')
+  const [senha, setSenha]=useState('')
+
+  useEffect(()=>{},[nome, email, senha])
+
+
+  async function _data(){
+
+    auth.currentUser.getIdTokenResult(true)
+        .then(e=>{
+
+            let _AuthTime=e.authTime
+            let _Token=e.token
+            let _SignProvider=e.signInProvider
+            let _ExpTime=e.expirationTime
+            let ID=auth.currentUser.uid
+
+            addDoc(collection(db,"User"),{
+                ID,
+                nome,
+                email, 
+                _AuthTime, 
+                _SignProvider,
+                _Token,
+                _ExpTime
+            })
+            .catch(e=>console.warn(e))
+        })
+        
+    }
+
+
+
+  async function cadastrar(){
+
+    await createUserWithEmailAndPassword(auth, email, senha)
+      .then(function(e){
+        _data()
+        limpar()
+        props.navigation.navigate("login")
+      })
+      .catch(function(e){
+        console.warn(e)
+      })
+      
+  }
+
+  function limpar(){
+    setEmail('')
+    setSenha('')
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+
+      <Title title="Cadastro" />
+
+      <View style={styles.internalContainer}>
+          <TextInput placeholder='nome'
+                     color='#2196f3'
+                     value={nome}
+                     onChangeText={(e)=>{setNome(e)}}/> 
+
+          <TextInput placeholder='email'
+                     color='#2196f3'
+                     value={email}
+                     onChangeText={(e)=>{setEmail(e)}}/> 
+
+          <TextInput placeholder='senha'
+                     color='#2196f3'
+                     secureTextEntry
+                     value={senha}
+                     onChangeText={(e)=>{setSenha(e)}}/> 
+
+
+        <Button style={styles.btn}
+                variant="outlined"
+                title="cadastrar" 
+                color='#2196f3'
+                trailing={props => <Icon name="send" {...props} />} 
+                onPress={cadastrar}
+                />
+
+        <Text style={styles.footerText}
+                onPress={()=>props.navigation.navigate("login")}>
+                  possui conta? faça login aqui.
+        </Text>
+
+      </View>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems:'center',
+    justifyContent:'space-evenly',
+    backgroundColor: 'whitesmoke',
+    width:'100%',
+    height:'60%',
+  },text:{
+      color:'white',
+  },internalContainer:{
+      width:'80%',
+      height:'40%',
+  },btn:{
+      marginTop:'5%',
+  },footerText:{
+      width:'100%',
+      textAlign:'center',
+      marginTop:15,
+      color:'#2976E6',
+  }
+});
---
+++ app @v1.0.5/registroDeMutantes/src/views/registros/index.jsx
@@ -0,0 +1,111 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+import { TextInput, Button, } from '@react-native-material/core';
+
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+
+import { useEffect, useState } from 'react';
+
+import { auth, db } from '../../services'
+import { addDoc, collection } from 'firebase/firestore';
+
+import { signOut } from 'firebase/auth';
+
+
+export default (props) =>{
+
+ 
+  const [ID, setID]=useState('')
+  const [email, setEmail]=useState('')
+  
+  useEffect(()=>{
+
+        let userEmail=auth.currentUser.email
+        setEmail(userEmail)
+
+  },[email])
+
+  function sair(){
+      signOut(auth).then(function(e){
+          props.navigation.navigate("login")
+      }).catch(e=>console.warn(e))
+      
+  }
+
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.inboundContainer}>
+
+
+            <View style={styles.form}>
+                <Text style={styles.userData}>{email}</Text>
+                 
+                <View style={styles.inbounForm}>
+                  <Text>formulary</Text>
+                </View>
+            </View>
+
+            <View style={styles.floatBtn}>
+              <Button style={styles.btn}
+                  variant="outlined"
+                  title="sair" 
+                  color='#2196f3'
+                  trailing={props => <Icon name="exit-to-app" {...props} />} 
+                  onPress={sair}
+                  />
+            </View>
+
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width:'100%',
+    height:'100%',
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
+  },inboundContainer:{
+      width:'88%',
+      height:'100%',
+      backgroundColor:'#fff',
+      alignItems:'center',
+      justifyContent:'center',
+      padding:5,
+  },text:{
+      fontSize:30,
+      color:'white'
+  },userData:{
+      flex:9,
+      marginTop:10,
+      fontSize:15,
+      color:'#2976E6',
+      fontWeight:'bold',
+  },btn:{
+      marginTop:100,
+  },floatBtn:{
+      alignItems:'flex-end',
+      justifyContent:'flex-end',
+      width:'100%',
+      padding:10,
+  },form:{
+      height:'80%',
+      width:'100%',
+      backgroundColor:'whitesmoke',
+      alignItems:'center',
+      justifyContent:'center',
+  },inbounForm:{
+      flex:100,
+      backgroundColor:'rgba(0,0,0,0.1)', 
+      alignItems:'center',
+      justifyContent:'center',
+      width:'100%',
+      borderBottomEndRadius:6,
+      borderBottomLeftRadius:6,
+  },btn:{
+    
+  }
+});
